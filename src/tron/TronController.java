b7605ee763e8cb79c7f9f22839b5fd325e5a3fcf
Silvan Laube <silvanlaube@hotmail.com>
2017-01-02 14:04:21 +0100

    2player mode

--- src/tron/TronController.java
+++ src/tron/TronController.java
@@ -26,18 +26,24 @@ public class TronController extends DefaultController {
 	private static final double MOVE_PER_SECOND = 0.66;
 	
 	private Vec3 direction = new Vec3(0, -1, 0);
+	private Vec3 direction2 = new Vec3(0, -1, 0);
 	private Vec3 position = Vec3.ZERO;
+	private Vec3 position2 = Vec3.ZERO;
 	private final Mat4 scale = Mat4.scale(0.005f);
 	private Mat4 rotation = Mat4.rotate(0, Vec3.Z);
-	private final List<IMesh> falcon;
+	private Mat4 rotation2 = Mat4.rotate(0, Vec3.Z);
+	private final List<IMesh> falcon, falcon2;
 	private final BoundingBox bbBuilding, bbElevator;	
 	
 	private boolean fixCameraPos = false;
 	private boolean hasTurned = false;
+	private boolean hasTurned2 = false;
 	private int ticksToIgnoreCamMove = 0;
+	private int ticksToIgnoreCamMove2 = 0;
 
-	public TronController(List<IMesh> falcon, BoundingBox bbBuilding, BoundingBox bbElevator) {
+	public TronController(List<IMesh> falcon, List<IMesh> falcon2, BoundingBox bbBuilding, BoundingBox bbElevator) {
 		this.falcon = falcon;
+		this.falcon2 = falcon2;
 		this.bbBuilding = bbBuilding;
 		this.bbElevator = bbElevator;
 	}
@@ -54,6 +60,16 @@ public class TronController extends DefaultController {
 		case GLFW.GLFW_KEY_LEFT:
 			turn(90);
 			hasTurned = true;
+			break;
+		//case GLFW.GLFW_KEY_W:
+		//case GLFW.GLFW_KEY_S:
+		case GLFW.GLFW_KEY_D:
+			turn2(-90);
+			hasTurned2 = true;
+			break;
+		case GLFW.GLFW_KEY_A:
+			turn2(90);
+			hasTurned2 = true;
 			break;
 		case GLFW.GLFW_KEY_H:
 			printHelp(HELP);
@@ -78,6 +94,18 @@ public class TronController extends DefaultController {
 			direction = direction.add(new Vec3(1, angle > 0 ? -1 : 1, 0));
 		}
 	}
+	private void turn2(int angle) {
+		rotation2 = Mat4.multiply(rotation2, Mat4.rotate(angle, Vec3.Z));
+		if(direction2.y > 0) {
+			direction2 = direction2.add(new Vec3(angle > 0 ? -1 : 1, -1, 0));
+		} else if(direction2.y < 0) {
+			direction2 = direction2.add(new Vec3(angle > 0 ? 1 : -1, 1, 0));
+		} else if(direction2.x > 0) {
+			direction2 = direction2.add(new Vec3(-1, angle > 0 ? 1 : -1, 0));
+		} else {
+			direction2 = direction2.add(new Vec3(1, angle > 0 ? -1 : 1, 0));
+		}
+	}
 	
 	private void detectCollisions() {
 	    BoundingBox bbFalcon = new BoundingBox();
@@ -98,10 +126,10 @@ public class TronController extends DefaultController {
 		double dt = (time - time_last) * MOVE_PER_SECOND;
 		time_last = time;
 		
-		System.out.println(dt);
-		
-		
-		
+		if(getViews().size() < 2){
+			return;
+		}
+
 		Mat4 tr = Mat4.multiply(scale, rotation);
 		position = position.add(direction.scale((float)dt));
 		falcon.forEach(mesh -> {
@@ -109,35 +137,56 @@ public class TronController extends DefaultController {
 			mesh.setPosition(position);
 		});
 		
+		Mat4 tr2 = Mat4.multiply(scale, rotation2);
+		position2 = position2.add(direction2.scale((float)dt));
+		falcon2.forEach(mesh -> {
+			mesh.setTransform(tr2);
+			mesh.setPosition(position2);
+		});
+		
 		detectCollisions();
 		
-		ICamera cam = getCamera(getCurrentView());
+		ICamera cam = getCamera(getViews().get(0));
+		ICamera cam2 = getCamera(getViews().get(1));
+		
+		/*ICamera cam = getCamera(getCurrentView());
+		ICamera cam2 = getCamera(getCurrentView());*/
 
 		if(hasTurned) {
 		    hasTurned = false;
 		    ticksToIgnoreCamMove = 100;
 		}
+		if(hasTurned2) {
+		    hasTurned2 = false;
+		    ticksToIgnoreCamMove2 = 100;
+		}
 		
-		if(ticksToIgnoreCamMove == 0) {
-		    if(fixCameraPos)
-	            cam.setPosition(new Vec3(0, 0, 5));
-	        else {
-	            cam.setPosition(position.add(new Vec3(-direction.x, -direction.y, 0.5)));
-	            BoundingBox bbFalcon = new BoundingBox();
-	            /*for (IMesh mesh : falcon) {
-	                bbFalcon.add(mesh.getBounds());
-	            }
-	            System.out.println(bbFalcon.getCenter().subtract(cam.getPosition()));*/
-	        }
+		if(fixCameraPos) {
+			cam.setPosition(new Vec3(0, 0, 5));
+			cam2.setPosition(new Vec3(0, 0, 5));
 		} else {
-		    Vec3 oldCamPos = cam.getPosition();
-		    Vec3 futureCamPos = position.add(new Vec3(-direction.x, -direction.y, 0.5));
-		    Vec3 difference = futureCamPos.subtract(oldCamPos);
-		    difference = difference.scale((1/(float) ticksToIgnoreCamMove--));
-		    
-		    cam.setPosition(oldCamPos.add(difference));
+			if(ticksToIgnoreCamMove == 0) {
+		        cam.setPosition(position.add(new Vec3(-direction.x, -direction.y, 0.5)));
+			} else {
+			    Vec3 oldCamPos = cam.getPosition();
+			    Vec3 futureCamPos = position.add(new Vec3(-direction.x, -direction.y, 0.5));
+			    Vec3 difference = futureCamPos.subtract(oldCamPos);
+			    difference = difference.scale((1/(float) ticksToIgnoreCamMove--));
+			    cam.setPosition(oldCamPos.add(difference));
+			}
+			
+			if(ticksToIgnoreCamMove2 == 0) {
+				cam2.setPosition(position2.add(new Vec3(-direction2.x, -direction2.y, 0.5)));
+			} else {
+			    Vec3 oldCamPos = cam2.getPosition();
+			    Vec3 futureCamPos = position2.add(new Vec3(-direction2.x, -direction2.y, 0.5));
+			    Vec3 difference = futureCamPos.subtract(oldCamPos);
+			    difference = difference.scale((1/(float) ticksToIgnoreCamMove2--));
+			    cam2.setPosition(oldCamPos.add(difference));
+			}
 		}
 		
 		cam.setTarget(position.add(new Vec3(0, 0, 0.25)));
+		cam2.setTarget(position2.add(new Vec3(0, 0, 0.25)));
 	}
 }
